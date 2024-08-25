fddc8092c814fef3d4b3a09cb5d1b18491e44dda
HoDzha <hodzha@gmail.com>
2024-08-25 22:12:25 +0500

    Update lesson.py

--- lesson.py
+++ lesson.py
@@ -3,3 +3,10 @@ pygame.inint()
 window_size = (800, 600)
 screen = pygame.display.set_mode(window_size)
 pygame.display.caption = "Тестовый проект"
+run = True
+
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    pygame.display.update()
